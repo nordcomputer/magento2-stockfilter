fa0209a8a55601b6ebc4c0cec567228d640a97f2
Mario <mario@nordcomputer.de>
2021-07-12 13:26:41 +0200

    module creation

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Nordcomputer_Stockfilter', __DIR__);
+
